6d42e1e50ca0ab23f0d567d47f211e6f848e9288
mrs <863597281@qq.com>
2017-04-22 15:26:46 +0800

    Initial commit 完美解决  ---修改系统源码解决 TabLayout无法修改指示器宽度的问题

---
+++ app/src/main/java/com/mrs/materialtablayout/MainActivity.java
@@ -0,0 +1,164 @@
+package com.mrs.materialtablayout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialTabLayout materialTabLayout;
+    android.support.design.widget.TabLayout tableLayout;
+    ViewPager materialVp;
+    ViewPager vp;
+
+
+    FrameLayout content;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        materialTabLayout = (MaterialTabLayout) findViewById(R.id.materialTab);
+        tableLayout = (android.support.design.widget.TabLayout) findViewById(R.id.Tab);
+        materialVp = (ViewPager) findViewById(R.id.materialVp);
+        vp = (ViewPager) findViewById(R.id.vp);
+
+        content = (FrameLayout) findViewById(R.id.content);
+        setUpMaterialTab();
+        setTab();
+    }
+
+    public void setUpMaterialTab() {
+
+        final ArrayList<View> mViewList = getViewList();
+        final ArrayList<String> mTitleList = getTitleList();
+
+
+        materialVp.setAdapter(new MyPagerAdapter(mViewList, mTitleList));
+        materialTabLayout
+                .setTabMargin(20)
+                //  .addTabs("tab1","tab2","tab3")
+               // .setBottomLineColor()
+                .setTabsFromPagerAdapter(materialVp.getAdapter())
+                .setOnMaterialTabSelectedListener(new MaterialTabLayout.MaterialTabSelectedListener() {
+                    @Override
+                    public void onTabSelected(MaterialTabLayout.Tab tab, boolean reSelected) {
+                        //materialVp.setCurrentItem(tab.getPosition());
+                        Toast.makeText(MainActivity.this, tab.getPosition() + "--" + reSelected, Toast.LENGTH_SHORT).show();
+                    }
+                }).setupWithViewPager(materialVp);
+               // .setSelectTab(0);
+
+    }
+
+    public void setTab() {
+
+        final ArrayList<View> mViewList = getViewList();
+        final ArrayList<String> mTitleList = getTitleList();
+
+       // vp.setAdapter(new MyPagerAdapter(mViewList, mTitleList));
+       // tableLayout.setupWithViewPager(vp);
+       // tableLayout.setTabsFromPagerAdapter(vp.getAdapter());
+        tableLayout.addTab(tableLayout.newTab().setText("tab1"));
+        tableLayout.addTab(tableLayout.newTab().setText("tab2"));
+        tableLayout.addTab(tableLayout.newTab().setText("tab3"));
+        tableLayout.addTab(tableLayout.newTab().setText("tab4"));
+        tableLayout.addTab(tableLayout.newTab().setText("tab5"));
+        tableLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                content.addView(mViewList.get(tab.getPosition()));
+                Toast.makeText(MainActivity.this, tab.getPosition() + "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        tableLayout.getTabAt(0).select();
+    }
+
+    @NonNull
+    private ArrayList<String> getTitleList() {
+        //添加页卡标题
+        final ArrayList<String> mTitleList = new ArrayList<>();
+        mTitleList.add("No:1");
+        mTitleList.add("No:2");
+        mTitleList.add("No:3");
+        mTitleList.add("No:4");
+        mTitleList.add("No:5");
+        return mTitleList;
+    }
+
+    @NonNull
+    private ArrayList<View> getViewList() {
+        LayoutInflater mInflater = LayoutInflater.from(this);
+        View view1 = mInflater.inflate(R.layout.viewpager_item1, null);
+        View view2 = mInflater.inflate(R.layout.viewpager_item2, null);
+        View view3 = mInflater.inflate(R.layout.viewpager_item3, null);
+        View view4 = mInflater.inflate(R.layout.viewpager_item4, null);
+        View view5 = mInflater.inflate(R.layout.viewpager_item5, null);
+
+        //添加页卡视图
+        final ArrayList<View> mViewList = new ArrayList<>();
+        mViewList.add(view1);
+        mViewList.add(view2);
+        mViewList.add(view3);
+        mViewList.add(view4);
+        mViewList.add(view5);
+        return mViewList;
+    }
+
+
+    class MyPagerAdapter extends android.support.v4.view.PagerAdapter {
+        private ArrayList<View> mViewList;
+        private ArrayList<String> mTitleList;
+
+        public MyPagerAdapter(ArrayList<View> mViewList, ArrayList<String> mTitleList) {
+
+            this.mViewList = mViewList;
+            this.mTitleList = mTitleList;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mViewList.get(position));//添加页卡
+            return mViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mViewList.get(position));//删除页卡
+        }
+
+        @Override
+        public int getCount() {
+            return mTitleList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitleList.get(position);//页卡标题
+        }
+    }
+}
